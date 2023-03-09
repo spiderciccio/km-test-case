fdc7d885449506e3a894e7d9f6b0b2489af605bc
Dmitry Poklonskiy <dimik@yandex.ru>
2023-03-09 16:55:31 +0400

    feat: Add gh-pages

---
+++ precache-manifest.a5766ffe389dbb6424b4013c2f2c06f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b23f30c225da37eb362db8d8f873fe",
+    "url": "/km-test-case/index.html"
+  },
+  {
+    "revision": "e2a52f9c68c08d37971e",
+    "url": "/km-test-case/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "495aa93d31f5d3b10d8f",
+    "url": "/km-test-case/static/js/2.958d9117.chunk.js"
+  },
+  {
+    "revision": "e2a52f9c68c08d37971e",
+    "url": "/km-test-case/static/js/main.51860aa4.chunk.js"
+  },
+  {
+    "revision": "b32ed5dc0b76a27014e7",
+    "url": "/km-test-case/static/js/runtime~main.4f8b1b0b.js"
+  }
+]);
